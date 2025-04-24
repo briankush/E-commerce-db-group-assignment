6d0215a42d51b1bb71d70dbdbbfaeac0d077d6a5
mose868 <moseskimani414@gmail.com>
2025-04-25 00:29:10 +0300

    add this

--- data _entries.sql
+++ data _entries.sql
@@ -25,3 +25,33 @@ VALUES
 (8, 'Maybelline', 'Cosmetics', '2024-01-08'),  
 (9, 'Patagonia', 'Outdoor clothing', '2024-01-09'),  
 (10, 'HP', 'Computers and printers', '2024-01-10');
+
+
+
+-- 3. Product Table
+INSERT INTO product_category (category_id, name, description) 
+VALUES 
+(1, 'Clothing', 'Apparel products'),
+(2, 'Electronics', 'Gadgets and devices'),
+(3, 'Shoes', 'Footwear'),
+(4, 'Accessories', 'Bags, hats, etc.'),
+(5, 'Home & Living', 'Home decor items'),
+(6, 'Sports', 'Sports equipment'),
+(7, 'Books', 'Fiction and non-fiction'),
+(8, 'Toys', 'Children’s toys'),
+(9, 'Beauty', 'Skincare and makeup'),
+(10, 'Furniture', 'Chairs, tables, etc.');
+
+-- 4. size categories 
+INSERT INTO size_category (size_category_id, category_name) 
+VALUES 
+(1, 'Clothing Sizes'),
+(2, 'Shoe Sizes'),
+(3, 'Hat Sizes'),
+(4, 'Screen Sizes'),
+(5, 'Furniture Dimensions'),
+(6, 'Bottle Sizes'),
+(7, 'Book Sizes'),
+(8, 'Toy Sizes'),
+(9, 'Bag Sizes'),
+(10, 'Jewelry Sizes');
